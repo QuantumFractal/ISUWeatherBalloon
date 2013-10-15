e69643cf6b61060a3a6722437696722d0319d802
Tom <quanta@iastate.edu>
2013-10-15 17:33:39 -0500

    Initial code commenting set up

---
+++ arducode/arducode.ino
@@ -0,0 +1,21 @@
+/* Arduino Weather Balloon Code
+      Purpose: to capture data on a weather balloon rig.
+      
+   Created by:
+   Thomas Moll and Michael Snook
+   15 October 2013
+*/
+
+
+
+void setup()
+{
+  // put your setup code here, to run once:
+  
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  
+}
